a5b62b737b7678da2f986672f3b8572f23a6249d
Raoni <raoni.silvestre@hotmail.com>
2024-02-08 15:13:40 -0300

    Add delete-all-polls route

---
+++ src/http/routes/delete-all-polls.ts
@@ -0,0 +1,17 @@
+import { FastifyInstance } from "fastify"
+import { prisma } from "./../../lib/prisma"
+import { z } from 'zod'
+
+
+export async function deletePolls(app: FastifyInstance) {
+
+  app.post('/polls/d', async (req, res) => {
+    
+    await prisma.vote.deleteMany()
+    await prisma.pollOption.deleteMany()
+    await prisma.poll.deleteMany()
+    
+    return "deleted"
+  })
+
+}

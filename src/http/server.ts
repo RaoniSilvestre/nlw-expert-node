aacb02f0b46102395a2fb7014753c413f54b24d5
Raoni <raoni.silvestre@hotmail.com>
2024-02-08 15:13:36 -0300

    Add deletePolls route to server.ts

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,10 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
+
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 import { voteOnPoll } from './routes/vote-on-poll'
+import { deletePolls } from './routes/delete-all-polls'
 
 const app = fastify()
 
@@ -18,6 +20,7 @@ app.register(getPoll)
 
 app.register(voteOnPoll)
 
+app.register(deletePolls)
 
 
 app.listen({ port: 3333}).then(() => {
